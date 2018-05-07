00d83f5b3641133c2f43b39c0fd93ef889d6128b
René Meier <rmeier@ipb-halle.de>
2018-05-07 15:23:19 +0200

    Adjust test script to new executable name.

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -jar $DIR/MassBank-web/MassBank-Project/MassBank-lib/target/MassBank-lib-0.0.1-SNAPSHOT-jar-with-dependencies.jar $@
+java -jar $DIR/MassBank-web/MassBank-Project/MassBank-lib/target/Validator-jar-with-dependencies.jar $@
 if [ $? -ne 0 ]
 then 
  exit 1
