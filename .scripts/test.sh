b2ba27dc0908de593a4a30563845e0ef0007c33e
René Meier <rmeier@ipb-halle.de>
2018-05-08 16:24:14 +0200

    Adjust to new executable distribution.

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -jar $DIR/MassBank-web/MassBank-Project/MassBank-lib/target/Validator-jar-with-dependencies.jar $@
+$DIR/MassBank-web/MassBank-Project/MassBank-lib/target/MassBank-lib-0.0.1-default/MassBank-lib-0.0.1/bin/Validator $@
 if [ $? -ne 0 ]
 then 
  exit 1
