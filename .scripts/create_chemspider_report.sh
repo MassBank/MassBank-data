88e4fdd1979dcf5562514850ceddfc569ae05458
meier-rene <rmeier@googlemail.com>
2018-12-20 13:23:36 +0100

    Add helper script to creat reports for Chemspider

---
+++ .scripts/create_chemspider_report.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+for x in */*.txt; do echo `grep ACCESSION $x| cut -d" " -f2` https://massbank.eu/MassBank/RecordDisplay.jsp?id=`grep ACCESSION $x| cut -d" " -f2` `grep 'CH\$IUPAC' $x| cut -d" " -f2`; done
