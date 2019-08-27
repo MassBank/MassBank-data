790c055ec7638472398fb56123c9533add96d715
René Meier <rmeier@ipb-halle.de>
2019-08-27 16:44:27 +0200

    Make travis master/dev aware

--- bump-version.sh
+++ bump-version.sh
@@ -1,2 +1,50 @@
 #!/bin/bash
 echo $1 > VERSION
+
+if $(echo $1 | grep -q SNAPSHOT); then 
+  echo Set version of development release to $1. This version will validate against the dev branch of MassBank-web.
+cat << 'EOF' > .travis.yml
+language: java
+cache:
+  directories:
+  - $HOME/.m2
+install:
+  - git clone --branch dev --depth 1 -q https://github.com/MassBank/MassBank-web.git .scripts/MassBank-web
+  - mvn -q -f .scripts/MassBank-web/MassBank-Project/MassBank-lib/pom.xml package
+script: 
+  - .scripts/validate.sh $TESTDIR
+env:
+EOF
+  for x in $(ls -d */);do
+    if [ "$x" = ".scripts/" ]; then
+      continue
+    fi
+    if [ "$x" = "figure/" ]; then
+      continue
+    fi
+    echo " - TESTDIR=$x" >> .travis.yml
+  done
+else
+  echo Set version of release to $1. This version will validate against the master branch of MassBank-web.
+cat << 'EOF' > .travis.yml
+language: java
+cache:
+  directories:
+  - $HOME/.m2
+install:
+  - git clone --depth 1 -q https://github.com/MassBank/MassBank-web.git .scripts/MassBank-web
+  - mvn -q -f .scripts/MassBank-web/MassBank-Project/MassBank-lib/pom.xml package
+script: 
+  - .scripts/validate.sh $TESTDIR
+env:
+EOF
+  for x in $(ls -d */);do
+    if [ "$x" = ".scripts/" ]; then
+      continue
+    fi
+    if [ "$x" = "figure/" ]; then
+      continue
+    fi
+    echo " - TESTDIR=$x" >> .travis.yml
+  done
+fi
