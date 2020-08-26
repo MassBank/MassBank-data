2cda10e9d4e2f9a78abb4ffdae6d60fd3e495b35
--help <eric.bach@aalto.fi>
2020-08-26 17:50:53 +0300

    Fix solvent information in 'Athens_Univ'

--- Athens_Univ/fix_entry_format.sh
+++ Athens_Univ/fix_entry_format.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo "Fix redundant flow gradient information"
-sed -i 's/0\/100 at 17.1-25 min/0\/100 at 25 min/' *.txt
+#sed -i 's/0\/100 at 17.1-25 min/0\/100 at 25 min/' *.txt
 
 echo "Unify the column name, e.g. stripping extra spaces"
-sed -i 's/ACQUITY  UPLC BEH Amide 1.7 um 2.1x100mm, Waters/ACQUITY UPLC BEH Amide 1.7 um 2.1x100mm, Waters/' *.txt
-sed -i 's/ACQUITY UPLC BEH Amide 1.7 um 2.1x100 mm, Waters/ACQUITY UPLC BEH Amide 1.7 um 2.1x100mm, Waters/' *.txt
-sed -i 's/ACQUITY UPLC BEH Amide 1.7 um 2.1x100mm, Water$/ACQUITY UPLC BEH Amide 1.7 um 2.1x100mm, Waters/' *.txt
+#sed -i 's/ACQUITY  UPLC BEH Amide 1.7 um 2.1x100mm, Waters/ACQUITY UPLC BEH Amide 1.7 um 2.1x100mm, Waters/' *.txt
+#sed -i 's/ACQUITY UPLC BEH Amide 1.7 um 2.1x100 mm, Waters/ACQUITY UPLC BEH Amide 1.7 um 2.1x100mm, Waters/' *.txt
+#sed -i 's/ACQUITY UPLC BEH Amide 1.7 um 2.1x100mm, Water$/ACQUITY UPLC BEH Amide 1.7 um 2.1x100mm, Waters/' *.txt
 
 echo "Fix solvent information"
-sed -i 's/Acetonitrile:WATER/Acetonitrile:Water/' *.txt
-sed -i 's/Acetonitrile:Water with 0.01/Acetonitrile:Water 95:5 with 0.01/' *.txt
+#sed -i 's/Acetonitrile:WATER/Acetonitrile:Water/' *.txt
+#sed -i 's/Acetonitrile:Water with 0.01/Acetonitrile:Water 95:5 with 0.01/' *.txt
+sed -i 's/Acetonitrile:Water 95:5 with 0.01 formic acid and 1mM ammonium formate/Acetonitrile:Water 95:5 with 0.01% formic acid and 1mM ammonium formate/' *.txt
