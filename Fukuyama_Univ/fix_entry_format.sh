1d0b63150a421ca54104493126c77a473c0480c4
René Meier <rmeier@ipb-halle.de>
2020-10-02 09:53:57 +0200

    Remove script

--- Fukuyama_Univ/fix_entry_format.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-# Information extracted from original publication: 	https://doi.org/10.5458/jag.57.199
-sed -i "s/AC\$CHROMATOGRAPHY: SOLVENT CH3CN\/H2O/AC\$CHROMATOGRAPHY: SOLVENT A CH3CN\nAC\$CHROMATOGRAPHY: SOLVENT B H2O/" *.txt
