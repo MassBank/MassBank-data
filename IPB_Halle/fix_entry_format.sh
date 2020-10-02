56bc06336dea841305f5baf691b7d495a4c20ff2
René Meier <rmeier@ipb-halle.de>
2020-10-02 13:50:32 +0200

    Fix file permission

--- IPB_Halle/fix_entry_format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-sed -i "s/AC\$CHROMATOGRAPHY: SOLVENT A:water(0.1%HCOOH)\/ B:acetonitril(0.1%HCOOH)/AC\$CHROMATOGRAPHY: SOLVENT A water with 0.1% formic acid\nAC\$CHROMATOGRAPHY: SOLVENT B acetonitrile with 0.1% formic acid/" *.txt
-
-sed -i -r "s/RETENTION_TIME [0-9]+.[0-9]+/& sec/" PN*.txt
