91f495daf2e4b4fcc052dd2d468324a3da12dbb3
René Meier <rmeier@ipb-halle.de>
2022-12-15 14:29:43 +0100

    Refactor run_travis.sh

--- .scripts/run_travis.sh
+++ .scripts/run_travis.sh
@@ -1,31 +1,17 @@
 haserror=0
-#diff --version
-#for i in  */*.txt; do   echo $i; done | sort | grep Metabolon > filelist.txt
-#echo filelist.txt
-#cat filelist.txt
-#grep Metabolon legacy.blacklist > legacy.blacklist2
-#echo legacy.blacklist
-#cat legacy.blacklist2 
-#diff -u filelist.txt legacy.blacklist2
-#echo nur legacy
-#diff --new-line-format="" --unchanged-line-format="" filelist.txt legacy.blacklist2 
-#exit 0
 
+find * -name MSBNK-*.txt > all_records.list
+grep -Fxv -f legacy.blacklist all_records.list > non_legacy.list
+grep -Fxv -f non_legacy.list all_records.list > legacy.list
 
-for i in  */*.txt; do 
- echo $i; 
-done | sort | split -d --additional-suffix .list -C 500K - records
+echo Validate legacy content
+.scripts/MassBank-web/MassBank-Project/MassBank-lib/target/MassBank-lib/MassBank-lib/bin/Validator -legacy $(cat legacy.list) || haserror=1
 
-for i in records*.list; do 
- echo Working on chunk $i
- echo Validate legacy content
- echo Files to process
- diff --old-line-format="" --new-line-format="" $i legacy.blacklist | grep Metabolon
- .scripts/MassBank-web/MassBank-Project/MassBank-lib/target/MassBank-lib/MassBank-lib/bin/Validator -legacy `diff --old-line-format="" --new-line-format="" $i legacy.blacklist` || haserror=1
- echo Validate nonlegacy content
- echo Files to process
- diff --new-line-format="" --unchanged-line-format="" $i legacy.blacklist | grep Metabolon
- .scripts/MassBank-web/MassBank-Project/MassBank-lib/target/MassBank-lib/MassBank-lib/bin/Validator `diff --new-line-format="" --unchanged-line-format="" $i legacy.blacklist` || haserror=1
+echo Validate nonlegacy content
+cat non_legacy.list | split -d --additional-suffix .list -C 500K - records
+for i in records*.list; do
+  .scripts/MassBank-web/MassBank-Project/MassBank-lib/target/MassBank-lib/MassBank-lib/bin/Validator $(cat $i) || haserror=1
 done
+
 rm records*.list
 exit $haserror
